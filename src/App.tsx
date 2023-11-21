d20fd7fc197dd40bf07458beaa3277aee577b811
Ryong <tmdfyd1995@gmail.com>
2023-11-21 23:42:14 +0900

    청첩장 데이터 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
 import classNames from 'classnames/bind';
+import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
+import FullScreenMessage from './components/shared/FullScreenMessage';
 
 const cx = classNames.bind(styles);
 
 function App() {
-  return <div className={cx('container')}>App</div>;
+  const [wedding, setWedding] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  // 1. wedding 데이터 호출
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다');
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        setWedding(data);
+      })
+      .catch((e) => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />;
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />;
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>;
 }
 
 export default App;
