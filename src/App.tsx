a467a1e0e80e9aafeb7abe0713f6ae6f662542c9
Ryong <tmdfyd1995@gmail.com>
2023-11-21 22:35:15 +0900

    불필요한 코드 삭제 및 스크립트 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 import classNames from 'classnames/bind';
 import styles from './App.module.scss';
 
 const cx = classNames.bind(styles);
 
 function App() {
-  return (
-    <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className={cx('container')}>App</div>;
 }
 
 export default App;
