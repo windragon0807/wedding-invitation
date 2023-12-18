829b8940cc1333d1dfb4514e376503f5a04cc670
Ryong <tmdfyd1995@gmail.com>
2023-12-18 15:38:11 +0900

    비디오 최적화

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -9,8 +9,8 @@ export default function Video() {
   return (
     <Section className={cx('container')}>
       <video autoPlay loop muted poster="/assets/poster.jpg">
-        <source src="/assets/main.webm" type="video/webm" />
-        <source src="/assets/main.mp4" type="video/mp4" />
+        <source src="/assets/banner.webm" type="video/webm" />
+        <source src="/assets/banner.mp4" type="video/mp4" />
       </video>
     </Section>
   );
