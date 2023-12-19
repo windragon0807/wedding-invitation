1e83159914801782b3356b3645d1d056666e4876
Ryong <tmdfyd1995@gmail.com>
2023-12-20 00:15:18 +0900

    데이터 fetch 로직 분리

--- src/App.tsx
+++ src/App.tsx
@@ -13,35 +13,12 @@ import Map from './components/sections/Map';
 import Contact from './components/sections/Contact';
 import Share from './components/sections/Share';
 import AttendCountModal from './components/AttendCountModal';
+import useWedding from './hooks/useWedding';
 
 const cx = classNames.bind(styles);
 
 export default function App() {
-  const [wedding, setWedding] = useState<Wedding | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-
-    fetch('http://localhost:8888/wedding')
-      .then(response => {
-        if (response.ok === false) {
-          throw new Error('청첩장 정보를 불러오지 못했습니다');
-        }
-
-        return response.json();
-      })
-      .then(data => {
-        setWedding(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+  const { wedding, loading, error } = useWedding();
 
   if (loading) {
     return <FullScreenMessage type="loading" />;
